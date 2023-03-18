14047e80a54a70036cad7bbbf63d158c23aafaf1
ozeiasmello <ozeiasmello@live.com>
2023-03-18 20:30:20 -0300

    estilizando a sidebar

--- src/components/Paragrafo/styles.ts
+++ src/components/Paragrafo/styles.ts
@@ -1,9 +1,8 @@
 import styled from 'styled-components'
-
 import { Props } from '.'
 
 export const P = styled.p<Props>`
-  font-size: 14px;
+  font-size: ${(props) => (props.fontSize ? `${props.fontSize}px` : '14px')};
   color: ${(props) => (props.tipo === 'principal' ? '#282a35' : '#949494')};
-  line-heigh: 22px;
+  line-height: 22px;
 `
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/ozeiasmello.png" />
-    <Titulo fontSize={20}>Ozeias Mello</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Ozeias Mello</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        ozeiasmello
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor FullStack Python
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
